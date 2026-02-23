2712168ebf6d3baf8248d68f8ceb5c6a67e064ed
Dhiraj-Hambir <dhirajhambir98@gmail.com>
2026-02-23 16:31:20 +0000

    commiting initial V1-EC2.tf

---
+++ V1-EC2.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+resource "aws_instance" "demo-server" {
+    ami = "ami-0b6c6ebed2801a5cb"
+    instance_type = "t3.micro"
+    key_name = "ddp"
+  
+}
